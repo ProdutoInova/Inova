cf0d10393c3cbb99247a70f46ad4d2c45f8d4aaf
Rafael Santos <rafaelnsantos1@gmail.com>
2017-11-09 19:49:46 -0200

    baseline refatorado

---
+++ target/generated-sources/annotations/br/cidade/data/entity/Cidade_.java
@@ -0,0 +1,14 @@
+package br.cidade.data.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-09T19:42:16")
+@StaticMetamodel(Cidade.class)
+public class Cidade_ { 
+
+    public static volatile SingularAttribute<Cidade, Integer> codigo;
+    public static volatile SingularAttribute<Cidade, String> nome;
+
+}
